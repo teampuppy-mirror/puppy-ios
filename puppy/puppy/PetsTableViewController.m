d778fa73d42f5440191af5fe90ba779040735a19
Eder Martins <dedehmartins@icloud.com>
2015-04-12 07:35:27 -0300

    Gogo 24 horas de programação!
    Nao para

--- puppy/puppy/PetsTableViewController.m
+++ puppy/puppy/PetsTableViewController.m
@@ -22,12 +22,14 @@
 
 - (void)viewDidLoad {
     _pets = [[NSMutableArray alloc]init];
+    self.view.backgroundColor = [UIColor lightGrayColor];
     NSDictionary * dicPets = [CMRequest get:@"http://puppy.app.hackinpoa.tsuru.io/pets"].data;
     NSArray * pets = [dicPets valueForKey:@"pets"];
     
     
     for(int i=0;i<pets.count-1;i++){
         NSDictionary * currentPet = pets[i];
+        NSLog(@"%@",currentPet);
         Pet * pet = [[Pet alloc]init];
         pet.urlMiniatura = [currentPet valueForKey:@"foto"];
         pet.especie = [currentPet valueForKey:@"especie"];
@@ -39,6 +41,11 @@
         pet.faixaEtaria = [currentPet valueForKey:@"idade"];
         pet.localizacao = [currentPet valueForKey:@"localizacao"];
         NSInteger sexo = ((NSNumber *)[currentPet valueForKey:@"sexo"]).integerValue;
+        
+        NSDictionary * protetor = [pet valueForKey:@"protetor"];
+        
+        pet.nomeProtetor = [protetor valueForKey:@"nome"];
+        pet.idDoProtetor = [protetor valueForKey:@"id"];
         
         NSLog(@"Sexo: %ld",(long)sexo);
         if(sexo == 1){//femea
@@ -105,23 +112,33 @@
     [cell addSubview:grayBackgroundMiniatura];
     imageViewMiniatura.alpha = 0.0;
     
-    UILabel * lblNome = [[UILabel alloc]initWithFrame:CGRectMake(130, 15, self.view.frame.size.width - 145, 36)];
+    UILabel * lblNome = [[UILabel alloc]initWithFrame:CGRectMake(130, 15, self.view.frame.size.width - 140, 36)];
+    lblNome.adjustsFontSizeToFitWidth = YES;
     lblNome.font = [UIFont fontWithName:@"HelveticaNeue-Medium" size:21];
     lblNome.text = pet.nome;
-    UILabel * lblEspeciePorte = [[UILabel alloc] initWithFrame:CGRectMake(130, 51, self.view.frame.size.width - 145, 36)];
+    UILabel * lblEspeciePorte = [[UILabel alloc] initWithFrame:CGRectMake(130, 51, (self.view.frame.size.width - 140)/2, 36)];
     lblEspeciePorte.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:17];
-    lblEspeciePorte.text = [NSString stringWithFormat:@"%@   %@",pet.especie,pet.porte];
+    lblEspeciePorte.text = [NSString stringWithFormat:@"%@",pet.especie];
+    lblEspeciePorte.adjustsFontSizeToFitWidth = YES;
+    
+    UILabel * lblPorte = [[UILabel alloc]initWithFrame:CGRectMake(130 + (self.view.frame.size.width - 140)/2, 51, (self.view.frame.size.width - 140)/2, 36)];
+    lblPorte.text = [NSString stringWithFormat:@"%@", pet.porte ];
+    lblPorte.font =[UIFont fontWithName:@"HelveticaNeue-Light" size:17];
+    
+    [cell addSubview:lblPorte];
+    lblPorte.adjustsFontSizeToFitWidth = YES;
+    
     
     [cell addSubview:lblEspeciePorte];
     
-    UILabel * lblCidade = [[UILabel alloc]initWithFrame:CGRectMake(160, 87, self.view.frame.size.width - 145, 36)];
+    UILabel * lblCidade = [[UILabel alloc]initWithFrame:CGRectMake(130, 87, self.view.frame.size.width - 145, 36)];
     lblCidade.text = pet.localizacao;
     lblCidade.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:17];
     [cell addSubview:lblCidade];
     
     UIImageView * pinImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"map-pin-hi.png"]];
     pinImageView.frame = CGRectMake(130, 89, 20, 25);
-    [cell addSubview:pinImageView];
+    //[cell addSubview:pinImageView];
     
     
     dispatch_queue_t myCustomQueue = dispatch_queue_create("com.example.MyCustomQueue", NULL);
